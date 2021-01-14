07562e0820f2fca6620a509848730966f6946e67
Sang-Martin <59576275+Sang-Martin@users.noreply.github.com>
2021-01-14 15:36:32 -0700

    Create ArtistController with DDL query. Create Filter Search Sample page

---
+++ ChinookDemoSolution/ChinookSystem/ViewModels/SelectionList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    //this class will be used as a generic container for data
+    //  that will load a dropdownlist (ddl)
+    //this value field will represent a interger primary
+    //the display field will represent the displayed string of the ddl
+    public class SelectionList
+    {
+        public int ValueField { get; set; }
+        public string DisplayField { get; set; }
+    }
+}
---
+++ ChinookDemoSolution/WebApp/SamplePages/SearchByDDL.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="Filter Search Demo" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="SearchByDDL.aspx.vb" Inherits="WebApp.SearchByDDL" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+</asp:Content>
