b0d7382172087b0dc2494dd8837805dc49d97738
Sang-Martin <59576275+Sang-Martin@users.noreply.github.com>
2021-01-18 13:53:34 -0700

    Creat Code First from Database

--- ChinookDemoSolution/ChinookSystem/Entities/Artist.cs
+++ ChinookDemoSolution/ChinookSystem/Entities/Artist.cs
@@ -1,34 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-#region Additional Namespaces
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-#endregion
-
 namespace ChinookSystem.Entities
 {
-    [Table("Artists")]
-    internal class Artist
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    internal partial class Artist
     {
         private string _Name;
 
-        [Key]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Artist()
+        {
+            Albums = new HashSet<Album>();
+        }
+
         public int ArtistId { get; set; }
 
-        //[Required(ErrorMessage = "Artist name is required.")]
-        [StringLength(120,ErrorMessage ="Artist name is limited to 120 characters.")]
-        public string Name
+        [StringLength(120)]
+        public string Name 
         {
             get { return _Name; }
             set { _Name = string.IsNullOrEmpty(value) ? null : value; }
         }
 
-        //navigatinal properties
-        // 1 to many relationship; create the many relationship in this entity
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Album> Albums { get; set; }
     }
 }
